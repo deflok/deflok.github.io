aa37cda9088387d1fab1fe0d42816d3c40125457
Денис <Deflok-812@mail.ru>
2026-04-27 16:26:05 +0900

    create next js app

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,22 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  // Включаем статический экспорт
+  output: "export",
+
+  // 2. Если репозиторий называется 'my-portfolio', то базовый путь должен быть '/my-portfolio'
+  basePath: '',
+
+  // (Опционально) Чтобы изображения из public работали корректно
+  assetPrefix: '',
+
+  // Добавляем слеш в конце URL для корректной работы
+  trailingSlash: true,
+
+  // Отключаем оптимизацию изображений (GitHub Pages не поддерживает)
+  images: {
+    unoptimized: true
+  }
 };
 
 export default nextConfig;
